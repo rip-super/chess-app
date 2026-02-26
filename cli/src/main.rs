28f1410e8d60688d8714bb209cf69df7b6e7376d
rip_super <sahildash7704@gmail.com>
2026-02-25 22:02:37 -0600

    added print_bitboard function and bitboard helpers

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,37 @@
+use engine::*;
+
+fn print_bitboard(board: u64) {
+    println!();
+
+    for rank in 0..8 {
+        for file in 0..8 {
+            let square = Square::new(rank * 8 + file).unwrap();
+
+            if file == 0 {
+                print!(" {}  ", 8 - rank);
+            }
+
+            print!(" {}", get_bit(board, square));
+        }
+
+        println!();
+    }
+
+    print!("\n     a b c d e f g h\n\n");
+
+    println!("     Bitboard: {board}");
+
+    println!();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut board = 0;
+
+    set_bit(&mut board, Square::E4);
+    set_bit(&mut board, Square::C3);
+    set_bit(&mut board, Square::F2);
+    print_bitboard(board);
+
+    pop_bit(&mut board, Square::E4);
+    print_bitboard(board);
 }
--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[repr(u8)]
+#[derive(Clone, Copy, Debug)]
+#[rustfmt::skip]
+pub enum Square {
+    A8, B8, C8, D8, E8, F8, G8, H8,
+    A7, B7, C7, D7, E7, F7, G7, H7,
+    A6, B6, C6, D6, E6, F6, G6, H6,
+    A5, B5, C5, D5, E5, F5, G5, H5,
+    A4, B4, C4, D4, E4, F4, G4, H4,
+    A3, B3, C3, D3, E3, F3, G3, H3,
+    A2, B2, C2, D2, E2, F2, G2, H2,
+    A1, B1, C1, D1, E1, F1, G1, H1,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Square {
+    pub fn new(idx: u8) -> Option<Self> {
+        if idx < 64 {
+            Some(unsafe { std::mem::transmute::<u8, Square>(idx) })
+        } else {
+            None
+        }
     }
+
+    pub fn idx(self) -> u8 {
+        self as u8
+    }
+}
+
+pub fn get_bit(board: u64, square: Square) -> u8 {
+    ((board >> square.idx()) & 1) as u8
+}
+pub fn set_bit(board: &mut u64, square: Square) {
+    *board |= 1 << square.idx()
+}
+pub fn pop_bit(board: &mut u64, square: Square) {
+    *board &= !(1 << square.idx())
 }
