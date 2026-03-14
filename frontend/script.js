9d0d7060f6da0761e5dee3af3ed53327d1b15b4f
rip_super <sahildash7704@gmail.com>
2026-03-14 11:40:37 -0500

    added move highlighting

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,44 @@
 import init, { ChessEngine } from "./pkg/wasm.js";
 
-function renderBoard(board, engine) {
+let selectedSq = null;
+let legalTargets = [];
+
+function getLegalTargets(engine, fromSq) {
+    return engine.legal_moves().filter(mv => mv.from_sq() === fromSq).map(mv => mv.to_sq());
+}
+
+function onSquareClick(sq, engine) {
+    if (selectedSq === sq) {
+        selectedSq = null;
+        legalTargets = [];
+    } else if (engine.piece_on(sq)) {
+        selectedSq = sq;
+        legalTargets = getLegalTargets(engine, sq);
+    } else {
+        selectedSq = null;
+        legalTargets = [];
+    }
+
+    renderBoard(board, engine);
+}
+
+function renderBoard(board, engine, invert = false) {
     board.innerHTML = "";
-    for (let rank = 7; rank >= 0; rank--) {
-        for (let file = 0; file < 8; file++) {
+    for (let row = 0; row < 8; row++) {
+        for (let col = 0; col < 8; col++) {
+            const rank = invert ? row : 7 - row;
+            const file = invert ? 7 - col : col;
+            const sqIndex = rank * 8 + file;
+
             const sq = document.createElement("div");
             sq.className = "sq " + ((rank + file) % 2 === 0 ? "dark" : "light");
 
-            const piece = engine.piece_on(rank * 8 + file);
+            if (sqIndex === selectedSq) sq.classList.add("selected");
+            if (legalTargets.includes(sqIndex)) sq.classList.add("legal");
+
+            sq.addEventListener("click", () => onSquareClick(sqIndex, engine));
+
+            const piece = engine.piece_on(sqIndex);
             if (piece) {
                 const img = document.createElement("img");
                 img.src = `pieces/${piece}.svg`;
@@ -23,7 +54,7 @@ function renderBoard(board, engine) {
 async function main() {
     await init();
 
-    const engine = new ChessEngine();
+    const engine = ChessEngine.from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1 ");
     const board = document.getElementById("board");
 
     renderBoard(board, engine);
